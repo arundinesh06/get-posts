9160c8eb7ac8e56246c03e283dc7573af2939b61
Arun Dinesh <arundinesh247@gmail.com>
2023-02-02 15:39:25 +0530

    new updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
